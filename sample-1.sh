cf11153d2d4779d1652937076736f419e5ea3870
chaitanya.varma <cvarma347@gmail.com>
2026-03-16 21:19:33 +0530

    msg

---
+++ sample-1.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+
+echo "variables to passed"
+
+var1=$1
+var2=$2
+
+echo "$@ are input args"
+
+echo "$* are input args"
+
+echo $PWD
+
+echo $USER
+
+echo $HOME
+
+echo "$$ is Process instance id"
+
+echo "$! is process instance id of previous"
+
+
+
