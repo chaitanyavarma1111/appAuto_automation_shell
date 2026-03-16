0bb40dc650643f26f53f0ff5c68455a8d5e734d0
chaitanya.varma <cvarma347@gmail.com>
2026-03-16 21:08:00 +0530

    chai

--- sample.sh
+++ sample.sh
@@ -2,3 +2,28 @@
 
 echo "application deployed successfully" 
 
+#VAR="chaitanya varma"
+
+#VAR="CHAITANYA VARMA"
+
+#var1=$1
+
+#var2=$2
+
+#echo "$var1 and $var2 are the best friends" 
+
+#echo " $VAR started shell practice" 
+
+echo "enter the values:" 
+
+read var1
+
+read -s var2
+
+echo "$1 and $2 are the values"
+
+echo "$var1 and $var2 are the read values"
+
+var=$(echo "value of application")
+
+echo $var + "alive"
