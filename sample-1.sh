7a4a7bbc2a54608933cd7797af65b8df2c982685
chaitanya.varma <cvarma347@gmail.com>
2026-03-16 21:47:59 +0530

    msg

--- sample-1.sh
+++ sample-1.sh
@@ -1,35 +1,34 @@
 #! /bin/bash
 
 
-echo "variables to passed"
+#echo "variables to passed"
 
-var1=$1
-var2=$2
+#var1=$1
+#var2=$2
 
-echo "$@ are input args"
+#echo "$@ are input args"
 
-echo "$* are input args"
+#echo "$* are input args"
 
-echo $PWD
+#echo $PWD
 
-echo $USER
+#echo $USER
 
-echo $HOME
+#echo $HOME
 
-echo "$$ is Process instance id"
+#echo "$$ is Process instance id"
 
-var3="string"
+#var3="string"
 
-var=$(($var1+$var2+$var3))
+#var=$(($var1+$var2+$var3))
 
-echo "$var is the value and $var3 is default zero"
+#echo "$var is the value and $var3 is default zero"
 
 var4=("sit","stand",0,1)
 
-echo "${var5[@]}"
+#echo "${var5[@]}"
 
-echo "${var5[0]} and ${var5[1]} and ${var5[2]} and ${var5[3]}"
-
+echo "${var5[0]}"
 
 
 
