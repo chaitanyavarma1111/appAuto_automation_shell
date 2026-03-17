350684495409dcdb98c523f85f9a0cec4096eec7
chaitanya.varma <cvarma347@gmail.com>
2026-03-17 19:11:52 +0530

    msg

--- cond.sh
+++ cond.sh
@@ -1,32 +1,51 @@
 #! /bin/bash
 
-var1=$1
-var2=$2
+# /* var1=$1
+# var2=$2
 
-if [ $(($var1 - $var2)) -gt 100 ]; then 
+# if [ $(($var1 - $var2)) -gt 100 ]; then 
 
-    echo "value is higher"
-else 
-    echo "value is lower"
+#     echo "value is higher"
+# else 
+#     echo "value is lower"
 
-fi 
+# fi 
 
-echo "value of var3 and var4"
-read var3
-read -s var4
+# echo "value of var3 and var4"
+# read var3
+# read -s var4
 
-if [ $(($var3 % $var4)) -gt 10 ]; then 
+# if [ $(($var3 % $var4)) -gt 10 ]; then 
 
-    echo "value is higher"
+#     echo "value is higher"
 
-elif [ $(($var3 % $var4)) -gt 20 ]; then
+# elif [ $(($var3 % $var4)) -gt 20 ]; then
 
-    echo "value is medium"
+#     echo "value is medium"
+
+# else
+
+#     echo "value is low"
+
+# fi 
+
+
+
+# var1=$1
+# var2=$2
+
+# echo "value of $var1 and $var2"
+
+if [ $USER -eq "root" ]; then 
+
+    echo $? 
 
 else
 
-    echo "value is low"
+    echo "not a root user"
 
-fi 
+fi
+
+
 
 
