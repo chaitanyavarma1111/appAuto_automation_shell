6b04dc15dadf817b6d0d04be1da25fbd4de1288c
chaitanya.varma <cvarma347@gmail.com>
2026-03-17 19:47:07 +0530

    msg

--- function.sh
+++ function.sh
@@ -11,13 +11,13 @@ var1=$1
 var2=$2
 
 if [ $var2 = "H" ] && [ $var1 -lt 100 ]; then 
-    echo "you are $G good $N"
+    echo -e "you are $G good $N"
 elif [ $var2 = "H"] && [ $var1 -gt 100 ]; then
-    echo "you are $R not at good speed $N keep it less then 100 for $Y orange for sometime $N"
+    echo -e "you are $R not at good speed $N keep it less then 100 for $Y orange for sometime $N"
 elif [ $var2 = "G"] && [ $var1 -gt 100 ]; then
-    echo "kindly stop your vehicle for while and give $Y cool start $N"
+    echo -e "kindly stop your vehicle for while and give $Y cool start $N"
 else
-    echo "Thanks for following rule $Y enjoy your trip $N"
+    echo -e "Thanks for following rule $Y enjoy your trip $N"
 fi
 
 
