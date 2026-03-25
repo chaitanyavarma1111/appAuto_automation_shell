b2a86761f50ac8e54c41bdb6cdd5857deecabffe
chaitanya.varma <cvarma347@gmail.com>
2026-03-26 01:53:22 +0530

    new

--- auto_instance.sh
+++ auto_instance.sh
@@ -9,7 +9,7 @@ DOMAIN="chaitanya.cloud"
 for instance in $@
 do
 
-    ins_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications   "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
+    ins_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications   "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance == "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $ins_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
