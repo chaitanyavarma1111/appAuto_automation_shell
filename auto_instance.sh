9cb5b54c507755fe4d70c75d02dc64e6ff7dd32d
chaitanya.varma <cvarma347@gmail.com>
2026-03-26 01:40:10 +0530

    new

--- auto_instance.sh
+++ auto_instance.sh
@@ -12,10 +12,10 @@ do
     ins_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications   "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
 
     if [ $instance == "frontend" ]; then
-        IP=$(aws ec2 describe-instance --instance-ids $ins_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $ins_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         record_name="$DOMAIN"
     else
-        IP=$(aws ec2 describe-instance --instance-ids $ins_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $ins_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         record_name="$instance.$DOMAIN"
     fi
 
