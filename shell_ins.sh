9fec681fabb8b842a1bba3e23bd51fa197875cae
chaitanya.varma <cvarma347@gmail.com>
2026-06-17 08:56:54 +0530

    message

--- shell_ins.sh
+++ shell_ins.sh
@@ -1,23 +1,43 @@
 #! /bin/bash
 
-read -s Password
+# echo "enter the password"
 
-echo "Password is $Password"
+# read -s Password
 
-var1=$1
+# echo "Password is $Password"
 
-echo "enter the value of var2"
-read var2
+# var1=$1
+
+# echo "enter the value of var2"
+# read var2
 
 
 
-if [ $(($var1 - $var2)) -gt 100 ]; then 
-    echo "value is more then 100"
-elif [ $(($var1 - $var2)) -gt 50 ]; then
-    echo "valus is gt than 50"
-else
-    echo "value is less then 50"
-fi
+# if [ $(($var1 - $var2)) -gt 100 ]; then 
+#     echo "value is more then 100"
+# elif [ $(($var1 - $var2)) -gt 50 ]; then
+#     echo "valus is gt than 50"
+# else
+#     echo "value is less then 50"
+# fi
+
+
+var=$(id -u)
+
+echo $var
+echo "$? is the status"
+echo $USER
+echo "$? is the status"
+echo $PWD
+echo "$? is the status"
+echo $HOME
+echo "$? is the status"
+echo $CPU
+echo "$? is the status"
+
+
+
+
 
 
 
