85c4273652f81c6e3304113f675fdaf845d2f3b0
chaitanya.varma <cvarma347@gmail.com>
2026-06-23 08:17:04 +0530

    fucntion

--- shell_ins.sh
+++ shell_ins.sh
@@ -25,23 +25,27 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-mkdir -p "/var/log/shell-script"
+logpath="/var/log/shell-script"
 
+logfile="shellscript.txt"
 
-$logfile="/var/log/shell-script/Shellscript.txt"
+logUpdate=$logpath/$logfile
+
+mkdir -p "$logpath"
+
 
 var=$(id -u)
 
-echo $var &>> $logfile
-echo "$? is the status" &>>$logfile
-echo $USER &>> $logfile
-echo "$? is the status" &>>$logfile
-echo $PWD &>> $logfile
-echo "$? is the status" &>>$logfile
-echo $HOME &>> $logfile
-echo "$? is the status" &>>$logfile
-echo $(df -h) &>> $logfile
-echo "$? is the status" &>>$logfile
+echo $var &>>$logUpdate
+echo "$? is the status" &>>$logUpdate
+echo $USER &>> $logUpdate
+echo "$? is the status" &>>$logUpdate
+echo $PWD &>> $logUpdate
+echo "$? is the status" &>>$logUpdate
+echo $HOME &>>$logUpdate
+echo "$? is the status" &>>$logUpdate
+echo $(df -h) &>>$logUpdate
+echo "$? is the status" &>>$logUpdate
 
 if [ $? -eq 0 ]; then
     echo -e "$R All the details are updated as paer the requirements $N"
