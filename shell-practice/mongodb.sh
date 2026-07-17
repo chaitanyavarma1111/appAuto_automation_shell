a29ff3e1fea0bc9edb657fe98f5217ecc58d72a0
chaitanya.varma <cvarma347@gmail.com>
2026-07-17 20:34:43 +0530

    commit the data

---
+++ shell-practice/mongodb.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p "$LOGS_FOLDER"
+echo "Script Started execution at $(date)" | tee -a $LOG_FILE
+
+
+if [ $USERID -ne 0 ]; then 
+    echo "ERROR:: Please run the script with the root access"
+    exit 1
+fi 
+
+VALIDATE(){
+
+    if [ $1 -eq 0 ]; then 
+        echo "$2 is $R is failure $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "$2 is $G is successful $N" | tee -a $LOG_FILE
+    fi
+}
+
+cp -p mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy the repo file"
+
+dnf install mongodb-org -y
+VALIDATE $? "Installing the mongodb" 
+
+systemctl enable mongod 
+VALIDATE $? "Enable Mongodb service"
+
+systemctl start mongod
+VALIDATE $? "Starting the Mongod service"
+
+sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Replacing the config file" 
+
+systemctl restart mongod
+VALIDATE $? "Restarting the service"
