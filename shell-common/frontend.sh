c4e00799b9b9960614601f03aa2c2a396871fb9d
chaitanya.varma <cvarma347@gmail.com>
2026-07-23 00:41:23 +0530

    commit the data

--- shell-common/frontend.sh
+++ shell-common/frontend.sh
@@ -1,17 +1,38 @@
 #! /bin/bash
 
+source ./common.sh
 
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
+user_check
+
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "disable nginx"
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "enable nginx"
 dnf install nginx -y
+VALIDATE $? "install nginx"
 
-systemctl enable nginx 
-systemctl start nginx 
 
-rm -rf /usr/share/nginx/html/* 
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enable nginx"
+systemctl start nginx &>>$LOG_FILE 
+VALIDATE $? "Start nginx"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+
+cd /usr/share/nginx/html &>>$LOG_FILE
+
+unzip /tmp/frontend.zip &>>$LOG_FILE
+
+VALIDATE $? "frontend application"
+
+cp $SCRIPT_PATH/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "nginx conf copied"
+
+systemctl restart nginx 
+VALIDATE $? "Restart completed "
+
+
 
