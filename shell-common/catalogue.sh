9fb5c1024bd2bec0f0623ea3c0449902ca75cad7
chaitanya.varma <cvarma347@gmail.com>
2026-07-22 10:48:36 +0530

    commit the data

---
+++ shell-common/catalogue.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+source ./common.sh
+appname=catalogue
+
+user_check
+app_setup
+nodejs_setup
+systemd_setup
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo 
+VALIDATE $? "Copy the repo file"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing the mongod"
+
+INDEX=$(mongosh mongodb.chaitanya.cloud --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+
+if [ $INDEX -le 0 ]; then 
+    mongosh --host mongodb.chaitanya.cloud </app/db/master-data.js
+else
+    echo -e "$G Database already exist $N" | tee -a $LOG_FILE 
+fi
+
