d76e07506243b841a15215f8284e91eefca4e1fb
chaitanya.varma <cvarma347@gmail.com>
2026-07-22 11:07:43 +0530

    commit the data

--- shell-common/catalogue.sh
+++ shell-common/catalogue.sh
@@ -8,7 +8,7 @@ app_setup
 nodejs_setup
 systemd_setup
 
-cp $SCRIPT_NAME/mongo.repo /etc/yum.repos.d/mongo.repo 
+cp $SCRIPT_PATH/mongo.repo /etc/yum.repos.d/mongo.repo 
 VALIDATE $? "Copy the repo file"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
