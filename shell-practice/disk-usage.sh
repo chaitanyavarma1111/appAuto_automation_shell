5ed27bcf2d279c5ab26328b2de0ea3706e35c297
chaitanya.varma <cvarma347@gmail.com>
2026-07-23 19:06:08 +0530

    to the root

--- shell-practice/disk-usage.sh
+++ shell-practice/disk-usage.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 DISK_STORAGE=$(df -hT | grep -v Filesystem)
-echo $DISK_STORAGE
+#echo $DISK_STORAGE
 DISK_TRESHOLD=2
 
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
@@ -13,7 +13,7 @@ do
     DISK_PER=$(echo $diskusage | awk '{print $6}' | cut -d "%" -f1)
     MOUNT_PATH=$(echo $diskusage | awk '{print $7}')
 
-    if [ DISK_PER -gt DISK_TRESHOLD ]; then
+    if [ $DISK_PER -gt $DISK_TRESHOLD ]; then
         MESSAGE+="The path is $MOUNT_PATH and space is $DISK_PER \n"
     fi 
 
