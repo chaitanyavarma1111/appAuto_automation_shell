82ceec1259cfe59329ba736396a7944af9990e35
chaitanya.varma <cvarma347@gmail.com>
2026-07-23 11:00:59 +0530

    to the root

--- shell-practice/delete-logs.sh
+++ shell-practice/delete-logs.sh
@@ -32,6 +32,6 @@ FIND_FILE=$(find $SOURCE_DIR -iname "*.log" -type f -mtime +10)
 while IFS=read -r filename 
 do
     echo "file to delete : $filename"
-    rm -f $filename
+    rm -rf $filename
 
 done <<< $FIND_FILE
